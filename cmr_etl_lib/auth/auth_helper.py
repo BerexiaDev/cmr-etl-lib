d3fdeeed12b92ba10d59915ed0319b2d66552d46
Karim Bourass <bourasskarim9@gmail.com>
2025-04-25 16:30:13 +0100

    Refactor AuthHelper to introduce get_logged_in_user_from_token method for improved token handling and user data retrieval. Simplify get_logged_in_user method and remove unused decode_auth_token function.

--- cmr_etl_lib/auth/auth_helper.py
+++ cmr_etl_lib/auth/auth_helper.py
@@ -1,17 +1,15 @@
 from cmr_etl_lib.auth.user import User
-from loguru import logger
-import jwt
-import os
+
 class AuthHelper:
-    def get_logged_in_user(new_request):
-        # get the auth token
-        authorization = new_request.headers.get('Authorization')
+    
+    @staticmethod
+    def get_logged_in_user(request):
+        authorization = request.headers.get('Authorization')
         auth_token = authorization.split(" ")[1]
         if auth_token:
             resp = User.decode_auth_token(auth_token)
             if not isinstance(resp, str):
                 user = User().load({'_id':resp['token']})
-                # TODO CHANGE THIS
                 response_object = {
                     'status': 'success',
                     'data': {
@@ -37,24 +35,23 @@ class AuthHelper:
                 'message': 'Provide a valid auth token.'
             }
             return response_object, 401
-        
-        
-def decode_auth_token(auth_token):
-    """
-    Decodes the auth token
-    :param auth_token:
-    :return: integer|string
-    """
-    try:
-        key = os.getenv("SECRET_KEY")
-        payload = jwt.decode(auth_token, key, algorithms=['HS256'])
-        return {"status": "success", "token": payload["sub"]}
-    except jwt.ExpiredSignatureError:
-        logger.error("Signature expired. Please log in again.")
-        return {
-            "status": "fail", 
-            "message": "Signature expired. Please log in again.",
-        }
-    except jwt.InvalidTokenError:
-        logger.error("Invalid token. Please log in again.")
-        return {"status": "fail", "message": "Invalid token. Please log in again."}
+    
+    @staticmethod
+    def get_logged_in_user_from_token(token):
+        resp = User.decode_auth_token(token)
+        if not isinstance(resp, str):
+            user = User().load({'_id':resp['token']})
+            response_object = {
+                'status': 'success',
+                'data': {
+                    'id': user.id,
+                    'email': user.email,
+                    'full_name': user.full_name,
+                    'is_active': user.is_active,
+                    'references': user.references,
+                    'process': user.process,
+                    'created_on': str(user.created_on),
+                    'role': user.role
+                }
+            }
+            return response_object
