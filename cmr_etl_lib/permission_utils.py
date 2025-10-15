a8dc36c18f521a019b486d31ada3e7401b45461d
Karim Bourass <bourasskarim9@gmail.com>
2025-10-15 10:32:50 +0100

    Update permission matrix to include additional roles for READ action in DATA_CLEANING module

--- cmr_etl_lib/permission_utils.py
+++ cmr_etl_lib/permission_utils.py
@@ -27,7 +27,12 @@ PERMISSION_MATRIX: Dict[AppModuleEnum, Dict[UserActionEnum, List[UserRoleEnum]]]
             UserRoleEnum.ADMIN.value
         ],
         UserActionEnum.READ.value: [
-            UserRoleEnum.ADMIN.value
+            UserRoleEnum.ADMIN.value,
+            UserRoleEnum.DATA_OWNER.value,
+            UserRoleEnum.DATA_STEWARD.value,
+            UserRoleEnum.SPONSOR.value,
+            UserRoleEnum.PERMANENT_CONTROL.value,
+            UserRoleEnum.MANAGEMENT_CONTROL.value
         ],
     },
     AppModuleEnum.DATA_CLEANING.value: {
